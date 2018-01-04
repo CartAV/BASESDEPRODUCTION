10db604f5bbfc2d1487fa89479dde23ff0999133
antoinefa <antoinefa>
2018-01-04 16:51:29 +0100

    Deleted recipe BASESDEPRODUCTION.compute_accidents_CLUV_dep

--- recipes/compute_accidents_CLUV_dep.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-    "current_name" AS "current_name",
-    "dep" AS "dep",
-    SUM("tue_nb") AS "tue_nb_sum",
-    COUNT(*) AS "count"
-  FROM "accidents_cluv_prepared"
-  WHERE "date" >= '2015-01-01 00:00:00+00' AND "nom_circonscription_police" != '' AND ("nom_circonscription_police" IS NOT NULL)
-  GROUP BY "current_name", "dep"
