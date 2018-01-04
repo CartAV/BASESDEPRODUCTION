37fd26b40ef9a0fda74d670275d2668d340c72ad
antoinefa <antoinefa>
2018-01-04 17:03:48 +0100

    Saved recipe: BASESDEPRODUCTION.compute_accidents_CLUV_prepared_by_date

--- recipes/compute_accidents_CLUV_prepared_by_date.sql
+++ recipes/compute_accidents_CLUV_prepared_by_date.sql
@@ -1,12 +1,24 @@
-    SELECT 
-        "dep_code",
-        FIRST_VALUE("gravite") OVER (PARTITION BY "dep" ORDER BY "gravite" DESC) AS "ville",
-        FIRST_VALUE("ville") OVER (PARTITION BY "dep" ORDER BY "gravite" DESC) AS "ville"
-      FROM (
-        SELECT 
+SELECT
+    "dep_code",
+    FIRST_VALUE("gravite") OVER (
+        PARTITION BY "dep"
+        ORDER BY
+            "gravite" DESC
+    ) AS "ville",
+    FIRST_VALUE("ville") OVER (
+        PARTITION BY "dep"
+        ORDER BY
+            "gravite" DESC
+    ) AS "ville"
+FROM
+    (
+        SELECT
             "dep" as "dep_code",
             "current_name" as "ville",
-            10*sum("tue_nb")+sum("hospitalise_nb") as "gravite"
-          FROM "accidents_cluv_prepared"
-          WHERE ("tue_nb" = 1) OR ("hospitalise_nb" = 1)
-        ) "dku__beforefirstlast"
+            10 * sum("tue_nb") + sum("hospitalise_nb") as "gravite"
+        FROM
+            "accidents_cluv_prepared"
+        WHERE
+            ("tue_nb" = 1)
+            OR ("hospitalise_nb" = 1)
+    ) "dku__beforefirstlast"
