9568d477ad3a6a2e3180d675a5afc8e0788af37b
dupontlau <dupontlau>
2019-03-21 17:16:26 +0000

    Saved recipe: BASESDEPRODUCTION.compute_szydloR5_1

--- recipes/compute_szydloR5_1.py
+++ recipes/compute_szydloR5_1.py
@@ -22,7 +22,7 @@ for input in inputs:
     export_folder = dataiku.Folder("v30qzlxb")
     export_path = export_folder.get_path()
     of = os.path.join(export_path, input + '.json.gz')
-    idf.to_json(of, compression='gzip', encoding='utf8', header=True)
+    idf.to_json(of, compression='gzip', encoding='utf8')
     size = idf.shape[0]
     print 'Wrote {} rows to {}'.format(size, of)
     osc = os.path.join(export_path, input + '.json')
