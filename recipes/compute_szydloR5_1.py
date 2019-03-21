c63ca05246ba81c7aacec92d8c8ff2ca56f603b7
dupontlau <dupontlau>
2019-03-21 14:33:19 +0000

    Saved recipe: BASESDEPRODUCTION.compute_szydloR5_1

--- recipes/compute_szydloR5_1.py
+++ recipes/compute_szydloR5_1.py
@@ -11,16 +11,18 @@ import csv
 import json
 from multiprocessing import Process, Queue
 
-inputs = ["es5_prod_accidents_copy", "es5_prod_pve_copy"]
-
+inputs = ["es5_prod_accidents", "es5_prod_accidents_vehicules", "es5_prod_accidents_usagers", 
+          "es5_prod_pve", 
+          "es5_prod_equipements_radar"
+         ]
 for input in inputs:
     ids = dataiku.Dataset(input)
     input_schema = ids.read_schema()
     idf = ids.get_dataframe()
     export_folder = dataiku.Folder("szydloR5")
     export_path = export_folder.get_path()
-    of = os.path.join(export_path, input + '.csv.gz')
-    idf.to_csv(of, mode='a', index=False, sep=',', compression='gzip', encoding='utf8', header=True)
+    of = os.path.join(export_path, input + '.json.gz')
+    idf.to_json(of, mode='a', index=False, sep=',', compression='gzip', encoding='utf8', header=True)
     size = idf.shape[0]
     print 'Wrote {} rows to {}'.format(size, of)
     osc = os.path.join(export_path, input + '.json')
