b3e5f2f956aa024b3b0a08c32b7f937059a0dbeb
dupontlau <dupontlau>
2019-03-27 13:37:08 +0000

    Saved recipe: BASESDEPRODUCTION.compute_export_dummy

--- recipes/compute_export_dummy.py
+++ recipes/compute_export_dummy.py
@@ -22,9 +22,9 @@ inputs = ["es5_prod_accidents", "es5_prod_accidents_vehicules", "es5_prod_accide
 files = [i + ".json" for i in inputs] + [i + "_schema.json" for i in inputs] 
 
 # OpenStack
-openstack_auth_url = "https://identity.api.pi.dsic.minint.fr/v3/auth/tokens" # "https://localhost:7700/v3/auth/tokens"
+openstack_auth_url = "https://192.168.3.13:7700/v3/auth/tokens" # "https://identity.api.pi.dsic.minint.fr/v3/auth/tokens"
 openstack_domain = "tech"
-swift_url = "https://object-store.api.pi.dsic.minint.fr/v1" # "https://localhost:7701/v1"
+swift_url = "https://192.168.3.13:7701/v1" # "https://object-store.api.pi.dsic.minint.fr/v1"
 swift_auth = "AUTH_373b0504876743f09427f84e4fd8fe9d"
 swift_container = "cartav-dev"
 openstack_user = "dupontla"
